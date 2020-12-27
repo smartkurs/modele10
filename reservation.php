f91335d31461a1611b30203d4d1191ca907ea5b0
smartkurs <62181488+smartkurs@users.noreply.github.com>
2020-12-27 12:56:31 +0600

    re

--- reservation.php
+++ reservation.php
@@ -1,3 +1,28 @@
+<?php
+// Free html5 templates : www.zerotheme.com
+
+$text = "<span style='color:red; font-size: 35px; line-height: 40px; magin: 10px;'>Error! Please try again.</span>";
+
+if(isset($_POST['name']))
+{
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$message=$_POST['message'];
+
+	$to = "youremail@gmail.com";
+	$subject = "Zerotheme - Testing Contact Form";
+	$message = " Name: " . $name ."\r\n Email: " . $email . "\r\n Message:\r\n" . $message;
+	 
+	$from = "Zerotheme";
+	$headers = "From:" . $from . "\r\n";
+	$headers .= "Content-type: text/plain; charset=UTF-8" . "\r\n"; 
+	 
+	if(@mail($to,$subject,$message,$headers))
+	{
+	  $text = "<span style='color:blue; font-size: 35px; line-height: 40px; margin: 10px;'>Your Message was sent successfully !</span>";
+	}
+}
+?>	
 <!DOCTYPE html>
 <!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
 <!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
@@ -8,9 +33,9 @@
     <!-- Basic Page Needs
   ================================================== -->
 	<meta charset="utf-8">
-	<title>websitedone</title>
-	<meta name="description" content="Responsive Html5 Css3 Templates | websitedone.com">
-	<meta name="author" content="www.websitedone.com">
+	<title>zItalyFood | Free Restaurant Html5 Templates</title>
+	<meta name="description" content="Free Responsive Html5 Css3 Templates | zerotheme.com">
+	<meta name="author" content="www.zerotheme.com">
 	
     <!-- Mobile Specific Metas
   ================================================== -->
@@ -64,7 +89,7 @@
 	<div class="site-title">
 		<div class="zerogrid">
 			<div class="row">
-				<h2 class="t-center">Название</h2>
+				<h2 class="t-center">Truely the best restaurant in town - The New York Times</h2>
 			</div>
 		</div>
 	</div>
@@ -83,122 +108,80 @@
 		</ul>
     </nav>
 	
-	<div class="zerogrid">
-		<div class="callbacks_container">
-			<ul class="rslides" id="slider4">
-				<li>
-					<img src="images/banner1.jpg" alt="">
-					<div class="caption">
-						<h2>We've got the best spareribs in town.</h2></br>
-						<p>Ваш текст здесь. </p>
-					</div>
-				</li>
-				<li>
-					<img src="images/banner2.jpg" alt="">
-					<div class="caption">
-						<h2>If food is an experience, then you'll find it at the restaurant.</h2></br>
-						<p>Ваш текст здесь.</p>
-					</div>
-				</li>
-				<li>
-					<img src="images/banner3.jpg" alt="">
-					<div class="caption">
-						<h2>Enjoy our take-away menu.</h2></br>
-						<p>Ваш текст здесь.</p>
-					</div>
-				</li>
+<!--////////////////////////////////////Container-->
+<section id="container" class="sub-page">
+	<div class="wrap-container zerogrid">
+		<div class="crumbs">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="reservation.html">Reservation</a></li>
 			</ul>
 		</div>
-	</div>
-	
-<!--////////////////////////////////////Container-->
-<section id="container" class="index-page">
-	<div class="wrap-container zerogrid">
-		<!-----------------content-box-1-------------------->
-		<section class="content-box box-1">
-			<div class="zerogrid">
-				<div class="row box-item"><!--Start Box-->
-					<h2>“Your awesome company slogan goes here, we have the best food around”</h2>
-					<span>Ваш текст здесь</span>
-				</div>
-			</div>
-		</section>
-		<!-----------------content-box-2-------------------->
-		<section class="content-box box-2">
-			<div class="zerogrid">
-				<div class="row wrap-box"><!--Start Box-->
-					<div class="header">
-						<h2>Welcome</h2>
-						<hr class="line">
-						<span>text text text text text</span>
-					</div>
-					<div class="row">
-						<div class="col-1-3">
-							<div class="wrap-col">
-								<div class="box-item">
-									<span class="ribbon">Menu Card<b></b></span>
-									<img src="images/menu.jpg" alt="">
-									<p>Ваш текст здесь</p>
-									<a href="#" class="button button-1">Detail</a>
-								</div>
-							</div>
-						</div>
-						<div class="col-1-3">
-							<div class="wrap-col">
-								<div class="box-item">
-									<span class="ribbon">Fast Food<b></b></span>
-									<img src="images/fast-food.jpg" alt="">
-									<p>Ваш текст здесь</p>
-									<a href="#" class="button button-1">Detail</a>
-								</div>
-							</div>
-						</div>
-						<div class="col-1-3">
-							<div class="wrap-col">
-								<div class="box-item">
-									<span class="ribbon">Reservation<b></b></span>
-									<img src="images/reservation.jpg" alt="">
-									<p>Ваш текст здесь</p>
-									<a href="#" class="button button-1">Detail</a>
-								</div>
-							</div>
+		<div id="main-content">
+			<div class="wrap-content">
+				<div class="row">
+					<!--Warning-->
+					<center><?php echo $text;?></center>
+					<!---->
+					<div class="col-1-3">
+						<div class="wrap-col">
+							<h3>Complete the Submission Form</h3>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p><br/>
+							<h3>Or Just Make a Call</h3>
+							<p>+6221 888 888 90 <br>
+								+6221 888 88891</p>
+							<p>info@yourdomain.com</p>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-1-3">
-							<div class="wrap-col">
-								<div class="box-item">
-									<span class="ribbon">Chef<b></b></span>
-									<img src="images/chef.jpg" alt="">
-									<p>Ваш текст здесь</p>
-									<a href="#" class="button button-1">Detail</a>
-								</div>
-							</div>
-						</div>
-						<div class="col-1-3">
-							<div class="wrap-col">
-								<div class="box-item">
-									<span class="ribbon">Preview<b></b></span>
-									<img src="images/preview.jpg" alt="">
-									<p>Ваш текст здесь</p>
-									<a href="#" class="button button-1">Detail</a>
-								</div>
-							</div>
-						</div>
-						<div class="col-1-3">
-							<div class="wrap-col">
-								<div class="box-item">
-									<span class="ribbon">Text Heading<b></b></span>
-									<img src="images/reservation.jpg" alt="">
-									<p>Ваш текст здесь</p>
-									<a href="#" class="button button-1">Detail</a>
+					<div class="col-2-3">
+						<div class="wrap-col">
+							<div class="contact">
+								<div id="contact_form">
+									<form name="contact" id="contact" method="post" action="reservation.php">
+										<label class="row">
+											<div class="col-1-2">
+												<div class="wrap-col">
+													<input type="text" name="name" id="name" placeholder="Enter name" required="required" />
+												</div>
+											</div>
+											<div class="col-1-2">
+												<div class="wrap-col">
+													<input type="email" name="email" id="email" placeholder="Enter email" required="required" />
+												</div>
+											</div>
+										</label>
+										<label class="row">
+											<div class="col-2-4">
+												<div class="wrap-col">
+												<input type="text" name="subject" id="subject" placeholder="Subject" required="required" />
+												</div>
+											</div>
+											<div class="col-1-4">
+												<div class="wrap-col">
+												<input type="date"  name="date" id="date" placeholder="Date"/>
+												</div>
+											</div>
+											<div class="col-1-4">
+												<div class="wrap-col">
+												<input type="time"  name="time" id="time" placeholder="Time"/>
+												</div>
+											</div>											
+										</label>
+										<label class="row">
+											<div class="wrap-col">
+												<textarea name="message" id="message" class="form-control" rows="4" cols="25" required="required"
+												placeholder="Message"></textarea>
+											</div>
+										</label>
+										<center><input class="sendButton" type="submit" name="Submit" value="Submit"></center>
+									</form>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</section>
+		</div> 
 	</div>
 </section>
 
@@ -212,7 +195,7 @@
 					<div class="row">
 						<img src="images/a-1.jpg">
 						<h5>Nick Roach</h5>
-						<p>Ваш текст здесь.</p>
+						<p>Pellentesque elementum leo et justo dapibus convalli. In justo nibh, congue nec dapibus ac, placerat eget sem. Nunc consequat felis non elit ultricies in varius massa laoreet. Aenean lectus nisl, ellentesque in fermentum sit amet, convallis a lorem condimentum mollis. Aenean lectus nisl, ellentesque in fermentum sit amet.</p>
 					</div>
 				</div>
 			</div>
@@ -237,7 +220,7 @@
 	</div>
 	<div class="copyright">
 		<div class="wrapper">
-			Copyright 2020 - Designed by <a href="https://www.websitedone.com">websitedone</a>
+			Copyright 2015 - Designed by <a href="https://www.zerotheme.com" title="free website templates">ZEROTHEME</a>
 			<ul class="quick-link f-right">
 				<li><a href="#">Privacy Policy</a></li>
 				<li><a href="#">Terms of Use</a></li>
@@ -250,26 +233,6 @@
 	<!-- js -->
 	<script src="js/classie.js"></script>
 	<script src="js/demo.js"></script>
-
-	<script src="js/jquery-1.11.3.min.js"></script>
-	<script src="js/responsiveslides.min.js"></script>
-	<script>
-	$(function () {
-	  // Slideshow 4
-	  $("#slider4").responsiveSlides({
-		auto: true,
-		pager: false,
-		nav: false,
-		speed: 500,
-		namespace: "callbacks",
-		before: function () {
-		  $('.events').append("<li>before event fired.</li>");
-		},
-		after: function () {
-		  $('.events').append("<li>after event fired.</li>");
-		}
-	  });
-	});
-	</script>
+	
 </div>
 </body></html>
